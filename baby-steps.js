e4b85ebedf82066364e07f5e06ce45940d4e5f63
Osvaldo <osvruizgar@gmail.com>
2022-06-22 21:39:06 -0400

    completed everything up to medium challenge and took screenshots

---
+++ baby-steps.js
@@ -0,0 +1,4 @@
+const stringToNumber = Number(process.argv[2]);
+const stringToNumber2 = Number(process.argv[3]);
+const stringToNumber3 = Number(process.argv[4]);
+console.log(stringToNumber + stringToNumber2 + stringToNumber3);
---
+++ logger.js
@@ -0,0 +1,9 @@
+// const url = "http://mylogger.io/log";
+
+// function logg(message) {
+//   //send an http request
+//   console.log(message);
+// }
+
+// module.exports.log = logg;
+//+++
